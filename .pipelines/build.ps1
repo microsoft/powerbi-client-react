2e35ec15ede1293fa9d0691157d13d55b0378918
Or Shemesh <orshemes@microsoft.com>
2022-07-11 10:36:15 +0300

    Verify dist folder doesn't have subfolders

--- .pipelines/build.ps1
+++ .pipelines/build.ps1
@@ -15,6 +15,10 @@ $exitCode += $LASTEXITCODE;
 
 Write-Host "start: Get dist folder files"
 & dir "dist"
+$dir = './dist'
+$hasAnySubdir = (Get-ChildItem -Force -Directory $dir).Count -gt 0
+Write-Host "dist folder has subdirs: $hasAnySubdir"
+$exitCode += If ($hasAnySubdir) {1} Else {0};
 Write-Host "Done: Get dist folder files"
 
 exit $exitCode

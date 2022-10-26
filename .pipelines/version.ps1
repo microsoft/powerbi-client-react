7d8d7c0309deb952092d39545c112f90f2eee0f0
Kotana Sai <v-kotanasai@microsoft.com>
2022-10-26 11:51:41 +0530

    Aligned yml file

--- .pipelines/version.ps1
+++ .pipelines/version.ps1
@@ -1,13 +1,12 @@
 try {
   # package.json is in root folder, while version.ps1 runs in .pipelines folder.
   $version = (Get-Content "package.json") -join "`n" | ConvertFrom-Json | Select -ExpandProperty "version"
-  $revision = $env:CDP_DEFINITION_BUILD_COUNT_DAY
+  $revision = $env:CDP_DEFINITION_BUILD_COUNT
   $buildNumber = "$version.$revision"
 
   Write-Host "Build Number is" $buildNumber
 
   Write-Host "##vso[task.setvariable variable=CustomBuildNumber]${buildNumber}"     # This will allow you to use it from env var in later steps of the same phase
-  Write-Host "##vso[build.updatebuildnumber]${buildNumber}"                         # This will update build number on your build
 }
 catch {
   Write-Error $_.Exception
